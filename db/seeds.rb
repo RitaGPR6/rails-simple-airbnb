36738ecfe74a4aa9ae339b95ecde3599954c3507
Rita Rodrigues <rita.gpr@gmail.com>
2024-11-13 17:10:03 +0000

    Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Creating flats'
+
+Flat.create!(
+  name: 'Modern City Apartment Manchester',
+  address: '25 Canal Street, Manchester M1 3EZ',
+  description: 'Stylish, contemporary apartment in the heart of Manchester. Open-plan living with one spacious double bedroom, a sleek kitchen, and a cozy lounge area.',
+  price_per_night: 90,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Charming Studio Flat in Edinburgh',
+  address: '15 George IV Bridge, Edinburgh EH1 1EW',
+  description: 'A quaint studio flat with stunning views of the city. Perfect for a couple, with a comfy bed, a modern bathroom, and a small but fully equipped kitchen.',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Beachside Retreat Brighton',
+  address: '2 Marine Parade, Brighton BN2 1TL',
+  description: 'Bright and airy flat with sea views. Two bedrooms, a cozy living area, and a fully equipped kitchen. Just steps away from the beach and the lively Brighton Pier.',
+  price_per_night: 110,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Historic Loft in Central York',
+  address: '10 Stonegate, York YO1 8AS',
+  description: 'Experience York in this charming, historic loft. One bedroom, exposed brick walls, and a rustic decor. Close to York Minster and the Shambles.',
+  price_per_night: 85,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Luxury Penthouse in London',
+  address: '1 The Shard, London SE1 9SG',
+  description: 'Enjoy breathtaking views of London from this high-rise penthouse. Two bedrooms, a spacious living area, and access to a private terrace. Perfect for luxury seekers.',
+  price_per_night: 300,
+  number_of_guests: 5
+)
+
+puts 'Done!'
